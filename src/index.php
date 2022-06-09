5e3b64f120a58244fd593cad5a26656a05c43002
KateBlkv <trapam@yandex.ru>
2022-06-09 22:34:00 +0300

    09/06/22

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use Katya\App\Users\Router;
+
+require_once './vendor/autoload.php';
+
+if (preg_match('/\.(css|js|gif|jpg|png|svg)$/', $_SERVER["REQUEST_URI"])) {
+    return false;
+}
+
+Router::start();
+
+// front controller
